ff4716816b753a05a312ad7466a836f681b5e877
Giuseppe Moschetta <giu.mos.dev@gmail.com>
2024-06-25 00:48:54 +0200

    create script to add seed sentences to mongodb

---
+++ seedSentences.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+require("./db");
+
+const Sentence = require("./models/Sentence.model");
+
+const sentences = [
+  " The mind is a powerful force; use it wisely.",
+  " Self-awareness is the first step to personal growth.",
+  " Emotions are neither good nor bad; they simply are. ",
+  " Our thoughts shape our reality.",
+  " Empathy bridges the gap between self and others.",
+];
+
+const seedSentences = async () => {
+  try {
+    await Sentence.deleteMany(); // clear the collection if needed
+    await Sentence.insertMany(sentences.map((text) => ({ text })));
+    console.log("Sentences have been added");
+    process.exit();
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+seedSentences();
